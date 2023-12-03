b18fe2c5999cb4449dc4008ff94331f84bdad6eb
Pasha-007 <m.muntahaa@gmail.com>
2023-12-03 14:32:26 +0600

    Login UI
    I have coded the Login page UI, Made two components "My Button", and "My Text Field" I will be reusing them in the Sign up page.

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -32,7 +32,7 @@ class IntroPage extends StatelessWidget {
                 ElevatedButton(
                     onPressed: (){
                       Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => const LoginPage()),
+                        MaterialPageRoute(builder: (context) => LoginPage()),
                       );
                     },
                     style: ElevatedButton.styleFrom(
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,13 +1,78 @@
 import 'package:flutter/material.dart';
+import '../components/my_button.dart';
+import '../components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //sign user in method
+  void signUserIn(){}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xff4D608A),
       appBar: AppBar(
         title: const Text('Login Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //username textfield
+            MyTextField(
+              controller: emailController,
+              hintText: 'Email',
+              obscureText: false,
+            ),
+            const SizedBox(height: 10,),
+            //password textfield
+            MyTextField(
+              controller: passwordController,
+              hintText: 'Password',
+              obscureText: true,
+            ),
+            const SizedBox(height: 10,),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.grey[200]),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            MyButton(
+              onTap: signUserIn,
+            ),
+            const SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Not a member?',
+                  style: TextStyle(color: Colors.grey[200]),
+                ),
+                const SizedBox(width: 4),
+                const Text(
+                  'Register now',
+                  style: TextStyle(
+                    color: Colors.blue, fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+
+          ],
+        ),
       ),
     );
   }
