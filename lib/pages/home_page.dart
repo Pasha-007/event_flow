19a40e8cce91140d878286291d18b864b61ba18d
Pasha-007 <m.muntahaa@gmail.com>
2023-12-03 19:19:19 +0600

    Bug Fixes.
    There was an Issue in Logging in on the user side, there was no option to log out, So I added a method for that.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser!;
+
+  void signUserOut(){
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: const Color(0xff4D608A),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: signUserOut,
+            icon: Icon(Icons.logout),
+          )
+        ],
+      ),
       body: Center(
         child: Text(
-          "Logged In",
+          "Logged In as " + user.email!,
+          style: TextStyle(fontSize: 20),
         ),
       ),
     );
